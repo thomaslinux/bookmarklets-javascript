ac4cda1495a7d0cc2c72210cb29e5a95a416e983
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-31 09:08:24 +0200

    Create wampserver_projects_links.user.js

---
+++ wampserver_projects_links.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Liens sur les projets php wampserver
+// @namespace   Violentmonkey Scripts
+// @match       http://localhost:8081/*
+// @grant       none
+// @version     2026.03.31.09.07
+// @author      thomaslinux9
+// @description Liens sur les projets php wampserver
+// ==/UserScript==
+javascript:(document.querySelectorAll(".projects li:not(.projectsdir)").forEach(projet => {
+  projet.innerHTML = '<a href="/' + projet.innerText + '">' + projet.innerText + '</a>'
+})) 
