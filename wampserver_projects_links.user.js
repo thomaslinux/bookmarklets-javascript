32e83b4c7b1dec0bb59051fcd4f28b89a5ac20ec
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-31 09:12:35 +0200

    Update wampserver_projects_links.user.js

--- wampserver_projects_links.user.js
+++ wampserver_projects_links.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name        wampserver_projects_links
-// @namespace   Violentmonkey Scripts
-// @match       http://localhost:8081/*
-// @grant       none
-// @version     2026.03.31.09.07
-// @author      thomaslinux9
-// @description Transforme les projets listés sur php wampserver en liens cliquables
+// @name            wampserver_projects_links
+// @namespace       Violentmonkey Scripts
+// @match           http://localhost:8081/*
+// @grant           none
+// @version         2026.03.31.09.07
+// @author          thomaslinux9
+// @description:fr  Transforme les projets listés sur php wampserver en liens cliquables
+// @description:en  Transforms each elements of the list of your projects in your wampserver into clickable links
 // ==/UserScript==
 javascript:(document.querySelectorAll(".projects li:not(.projectsdir)").forEach(projet => {
   projet.innerHTML = '<a href="/' + projet.innerText + '">' + projet.innerText + '</a>'
